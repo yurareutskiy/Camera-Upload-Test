23ed771493aec4ff17f11c68edd895ff2abce14a
yurareutskiy <yurareutskiy@gmail.com>
2015-10-24 10:43:52 +0300

    Initial Commit

---
+++ Camera Upload Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera Upload Test
+//
+//  Created by Reutskiy Jury on 10/24/15.
+//  Copyright © 2015 Styleru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
